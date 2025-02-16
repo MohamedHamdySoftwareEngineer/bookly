8385a4c6abd77e2cef5d33f365aebdb46927b343
TheBestSoftwareEngineer <MOHAMEDHAMDY192001@gmail.com>
2025-02-16 23:48:15 +0200

    search view done

--- lib/features/home/presentation/views/widgets/best_seller_list_view.dart
+++ lib/features/home/presentation/views/widgets/best_seller_list_view.dart
@@ -13,7 +13,26 @@ class BestSellerListView extends StatelessWidget {
       itemBuilder: (context, index) {
         return const Padding(
           padding: EdgeInsets.only(bottom: 20.0),
-          child: BestSellerListViewItem(),
+          child: BookListViewItem(),
+        );
+      },
+    );
+  }
+}
+
+class SearchResultListView extends StatelessWidget {
+  const SearchResultListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      physics: const NeverScrollableScrollPhysics(),
+      padding: EdgeInsets.zero,
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return const Padding(
+          padding: EdgeInsets.only(bottom: 20.0),
+          child: BookListViewItem(),
         );
       },
     );
--- lib/features/search/presentation/views/widgets/search_view_body.dart
+++ lib/features/search/presentation/views/widgets/search_view_body.dart
@@ -1,12 +1,52 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import '../../../../../core/utils/styles.dart';
+import '../../../../home/presentation/views/widgets/best_seller_list_view_item.dart';
+import 'custom_search_text_field.dart';
 
 class SearchViewBody extends StatelessWidget {
   const SearchViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 30),
+      child: Column(
+       crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomSearchTextField(),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            'Result',
+            // copyWith: create an instance from titleMedium and modify it
+            style: Styles.textStyle18,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Expanded(child: SearchResultListView()),
+        ],
+      ),
+    );
   }
 }
 
+class SearchResultListView extends StatelessWidget {
+  const SearchResultListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: EdgeInsets.zero,
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return const Padding(
+          padding: EdgeInsets.only(bottom: 20.0),
+          child: BookListViewItem(),
+        );
+      },
+    );
+  }
+}
