8df306eac61016a6d6a8f490db93419efd19f4ac
TheBestSoftwareEngineer <MOHAMEDHAMDY192001@gmail.com>
2025-02-20 11:30:10 +0200

    create api service

---
+++ lib/core/utils/api_service.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+// class that handle any api services
+import 'package:dio/dio.dart';
+
+class ApiService {
+  // fixed part of url, i will use in any place
+  final _baseUrl = 'https://www.googleapis.com/books/v1/';
+
+  final Dio dio;
+
+  ApiService({
+    required this.dio,
+  });
+
+  // Map<String, dynamic> → The result will be a map where: The keys are of type String - The values can be of any type (dynamic).
+  // Url = baseUrl + endPoint
+  // async,await to can recieve data
+  Future<Map<String, dynamic>> get({required String endPoint}) async {
+    // here the request
+    var response = await dio.get('$_baseUrl$endPoint');
+
+    return response.data;
+  }
+}
