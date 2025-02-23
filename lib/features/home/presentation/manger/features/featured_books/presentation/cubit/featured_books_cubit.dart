3a2c294aa09c1a7094d55cd4866fbd67d405fd34
TheBestSoftwareEngineer <MOHAMEDHAMDY192001@gmail.com>
2025-02-24 00:28:13 +0200

    create featured books method

--- lib/features/home/presentation/manger/features/featured_books/presentation/cubit/featured_books_cubit.dart
+++ lib/features/home/presentation/manger/features/featured_books/presentation/cubit/featured_books_cubit.dart
@@ -1,10 +1,31 @@
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-
 import '../../../../../../data/models/book_model/book_model.dart';
+import '../../../../../../data/repos/home_repo.dart';
 
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(
+    this.homeRepo,
+  ) : super(FeaturedBooksInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturedBooksLoading());
+
+    // result returns either failure or list of book model , then we can use fold method in result to control that who will returns
+    var result = await homeRepo.fetchFeaturedBooks();
+
+    result.fold(
+      (failures) {
+        emit(FeaturedBooksFailure(failures.errorMessage));
+      },
+      (books) {
+        emit(FeaturedBooksSuccess(books));
+      }
+    );
+  }
 }
